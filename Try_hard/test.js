2e40b45d6c4de917e1e9e1e2b7d77008d04292de
doantung2001 <clonenewby2000@gmail.com>
2022-08-06 15:55:34 +0700

    gui bt

---
+++ Try_hard/test.js
@@ -0,0 +1,160 @@
+//
+//     var tong;
+//     var dem=1;
+//     var i=1;
+//
+//     while (dem<=30){
+//         if(i%7==0)
+//         {
+//             dem+=1;
+//             tong+=i;
+//             document.write(i + "-")
+//         }
+//         i+=1;
+//     }
+// }
+
+    // const number = parseInt(prompt('Enter a positive number: '));
+    // let n1 = 0, n2 = 1, nextTerm;
+    //
+    // document.write('Fibonacci Series:');
+    // document.write(n1 + ";"); // print 0
+    // document.write(n2 + ";"); // print 1
+    //
+    // nextTerm= n1 + n2;
+    //
+    // for( let dem = 1; dem <= number;dem++)
+    // {
+    //     // print the next term
+    //     document.write(nextTerm + ";");
+    //
+    //     n1 = n2;
+    //     n2 = nextTerm;
+    //     nextTerm = n1 + n2;
+    //     nextTerm
+    //
+    // }
+
+
+
+
+
+
+// for (let i = 1; i<=100 ;i++){
+//     if(i % 3 === 0){
+//         document.write("Fizz" + ";");
+//     }else{
+//         if(i % 5 === 0){
+//             document.write("Buzz" + ";")
+//         }else {
+//             if(i % 15 === 0){
+//                 document.write("FIZZ BUZZ")
+//             } else {
+//                 document.write(i + "\n");
+//             }
+//         }
+//
+//     }
+//
+//
+// }
+
+// function {
+//     const n = parseInt(prompt('Enter a positive number: '));
+//     let i,j;
+//
+//     let Number = [1,2,3,4,5,6,7];
+//     for(i=0; i<n;i++){
+//         document.write(Number[i] + ';');
+//
+//     }
+//     let  tempSort;
+//
+//     for ( i = 0; i < n - 1; i++) {
+//         for ( j = i + 1; j <= n - 1; j++) {
+//             if (Number[i] < Number[j]) {
+//                 tempSort = Number[i];
+//                 Number[i] = Number[j];
+//                 Number[j] = tempSort;
+//             }
+//         }
+//     }
+//     document.write("Mảng sau khi sắp xếp là: ");
+//     for (let i = 0; i < n; i++) {
+//         document.write(Number[i] + ';');
+//     }
+// }
+
+// const N = parseInt(prompt('Enter a positive number: '));
+// // let i;
+// // let j;
+// // let mang = new Array[i][j];
+// // for(i=0;i<=N;i++){
+// //     for (j=0 ;j<=i;j++){
+// //         mang[i][j] = prompt("Hay nhap mang can nhap:");
+// //     }
+// // }
+// //
+// // for(i=0;i<=N;i++) {
+// //     for (j = 0; j <= i; j++) {
+// // document.write(mang[i][j] + " ;");
+// //     }
+// // }
+// //
+// //
+// //bai1
+// {
+//   let num1;
+//   let num2;
+//   let num3;
+//   num1 = prompt("Điền điểm Vật lý:",'');
+//   num2 = prompt("Điền điểm Hóa Học:",'');
+//   num3 = prompt("Điền điểm Sinh học:",'');
+//
+//   let DiemTb =((num1 + num2 + num3) / 3) ;
+//   let tong = (num1+ num2 + num3);
+//   alert("Điểm trung bình " + DiemTb);
+//   alert(tong);
+//  }
+
+//bai 2
+// {let num1;
+//  let num2;
+//  num1 = prompt("Hãy nhận độ C");
+//  num2 = num1*1.8 + 32;
+//  alert("ĐỘ F được chuyển là" + num2);}
+
+// bai 3
+//  let R;
+//  R = prompt('Hãy nhận bán kình hình tròn');
+//  let S = R*R*3.14;
+//  alert("Diện tích hình tròn la:" + S);
+//  let C = 2*R*3.14;
+//  alert("Chu vi hình tròn là: " + C);
+
+// Bai5
+
+
+var num1 = parent(document.getElementById("num1").value);
+var num2 = parent(document.getElementById("num2").value);
+
+function cong(num1,num2){
+    let tong = num1 + num2;
+    alert("tổng là :" + tong);
+}
+function tru(num1,num2){
+    let hieu = num1 + num2;
+    alert("Hiệu là :" + hieu);
+}
+function nhan(num1,num2){
+    let tich = num1 + num2;
+    alert("Tich là :" + tich);
+}
+function chia(num1,num2){
+    let thuong = num1 + num2;
+    alert("Thương là :" + thuong);
+}
+
+
+
+
